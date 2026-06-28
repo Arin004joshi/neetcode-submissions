69be3ffe7264a32391b61de598d3723b9ca5cce9
arin <139484606+Arin004joshi@users.noreply.github.com>
2026-06-28 19:55:16 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAlphaNumeric(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+    public boolean isPalindrome(String s) {
+        char[] arr = s.toLowerCase().toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            if (!isAlphaNumeric(arr[left])) {
+                left++;
+                continue;
+            }
+            if (!isAlphaNumeric(arr[right])) {
+                right--;
+                continue;
+            }
+            if (arr[left] == arr[right]) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
