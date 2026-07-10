e8110eef107def641e5b81582617dae9a7e9be8e
arin <139484606+Arin004joshi@users.noreply.github.com>
2026-07-10 13:20:07 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(
+                    c)) { // we are traversing in order to find the closing and opening brackets
+                if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
