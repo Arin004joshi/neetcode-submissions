636d16f690d19db2af35a618c22da763398bd258
arin <139484606+Arin004joshi@users.noreply.github.com>
2026-08-30 21:08:56 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0;
+        int r = x;
+        int res = 0;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m * m == x) {
+                return m;
+            } else if ((long) m * m > x) {
+                r = m - 1;
+            } else {
+                res = m;
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+}
