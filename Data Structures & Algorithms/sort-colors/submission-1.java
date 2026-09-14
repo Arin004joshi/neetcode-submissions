3ebcd243ba080b51ebf300d527d244f272773634
arin <139484606+Arin004joshi@users.noreply.github.com>
2026-09-14 11:58:32 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for (int i : nums) {
+            count[i]++;
+        }
+        int idx = 0;
+        for (int i = 0; i < count.length; i++) {
+            while(count[i]>0){
+                nums[idx++] = i;
+                count[i]--;
+            }
+        }
+    }
+}
